3e9e93204feeae60e4a645c5ce154560a064f4b5
Sadek-1801 <hmashrafujjaman@gmail.com>
2024-06-02 01:47:37 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 9000;
+const app = express();
+require('dotenv').config()
+
+// middle ware
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json())
+app.use(cookieParser())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qv5d3vd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const userCollection = client.db('fitfinesse').collection('users')
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+
+
+
+    app.put("/user", async(req, res)=>{
+        const user = req.body;
+        const query = {email: user?.email};
+        const isExist = await userCollection.findOne(query);
+        if(isExist){
+            return res.send(isExist)
+        }
+        const options = { upsert: true}
+        const updateDoc = {
+            $set:{
+                ...user,
+                time: Date.now(),
+            },
+        }
+        const result = await userCollection.updateOne(query, updateDoc, options);
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send("Fitfinesse server running")
+})
+app.listen(port, ()=>{
+    console.log(`Fitfinesse Server is running on ${port}`);
+})
+
+
+
